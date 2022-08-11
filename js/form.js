3a37ce9e15a264a6649a61a00e10168e03960cf9
Agus Eliel <aguseliel2@gmail.com>
2022-08-11 19:21:51 -0300

    fix form

--- js/form.js
+++ js/form.js
@@ -1,9 +1,9 @@
-var form = document.getElementById("form1");
+let form = document.getElementById("form1");
 
 async function handleSubmit(event) {
     event.preventDefault();
-    var status = document.getElementById("contacme__form__status");
-    var data = new FormData(event.target);
+    let status = document.getElementById("contacme__form__status");
+    let data = new FormData(event.target);
     fetch(event.target.action, {
         method: form.method,
         body: data,
@@ -13,14 +13,14 @@ async function handleSubmit(event) {
     })
         .then((response) => {
             if (response.ok) {
-                status.innerHTML = "Thanks for your submission!";
+                status.innerHTML = "Thanks for your message!";
                 form.reset();
             } else {
                 response.json().then((data) => {
                     if (Object.hasOwn(data, "errors")) {
                         status.innerHTML = data["errors"].map((error) => error["message"]).join(", ");
                     } else {
-                        status.innerHTML = "Oops! There was a problem submitting your form";
+                        status.innerHTML = "Oops! Sorry, your message couldn't by send";
                     }
                 });
             }
